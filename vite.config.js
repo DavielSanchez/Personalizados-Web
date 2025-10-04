45df14dccdcba15bfa392665c001b4f117c03f57
Daviel Alexander Sanchez <121512039+DavielSanchez@users.noreply.github.com>
2025-10-04 12:19:08 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/', // tu subcarpeta
+    base: '/personalizadosrd/', // tu subcarpeta
 })
