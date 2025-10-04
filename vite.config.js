4f7c525f59ee5e6797c774e85d682cbdc3c38c04
Daviel Alexander Sanchez <121512039+DavielSanchez@users.noreply.github.com>
2025-10-04 12:25:42 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    base: '/personalizadosrd/', // tu subcarpeta
-})
+  base: '/', // porque el subdominio ya apunta a /personalizadosrd
+  plugins: [react()]
+});
