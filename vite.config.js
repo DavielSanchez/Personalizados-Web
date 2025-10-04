27d325c33f45a35b5f163e08e01fb1ea3980948f
Daviel Alexander Sanchez <121512039+DavielSanchez@users.noreply.github.com>
2025-10-04 16:59:43 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/personalizadosrd/', // ✅ IMPORTANTE para que cargue bien en el subdominio
+  base: '/',
   plugins: [react()]
 })
