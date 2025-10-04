0e325f513e230f4fbb905fa8cc0b96994ea60cb7
Daviel_Sanchez <Davielsanchez14@gmail.com>
2025-10-04 18:04:28 -0400

    Fix: Arreglando importacion de imagen en la seccion de ofertas

--- src/Components/Offers.jsx
+++ src/Components/Offers.jsx
@@ -6,7 +6,7 @@ function Offers() {
         <div className="row px-xl-5">
             <div className="col-md-6 pb-4">
                 <div className="position-relative bg-secondary text-center text-md-right text-white mb-2 py-5 px-5">
-                    <img src="/public/img/offers-1.png" alt=""/>
+                    <img src="/img/offers-1.png" alt=""/>
                     <div className="position-relative" style={{ zIndex: 1 }}>
                         <h5 className="text-uppercase text-primary mb-3">10% off the all order</h5>
                         <h1 className="mb-4 font-weight-semi-bold">Summer Collection</h1>
