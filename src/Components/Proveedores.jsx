cfa596b8e5421b184c0600ba935ee738ee6f7571
Daviel Alexander Sanchez <121512039+DavielSanchez@users.noreply.github.com>
2025-10-04 17:46:07 -0400

    Update Proveedores.jsx

--- src/Components/Proveedores.jsx
+++ src/Components/Proveedores.jsx
@@ -1,20 +1,17 @@
-
 function Proveedores() {
   return (
-    <>
     <div className="container-fluid py-5">
-        <div className="row px-xl-5 d-flex">
-                <div className="vendor-item d-flex justify-content-between">
-                <img src="../../public/img/vendor-yuca.png" alt="" />
-                <img src="../../public/img/vendor-aremar.png" alt=""/>
-                <img src="../../public/img/vendor-moran.png" alt=""/>
-                <img src="../../public/img/vendor-graphic.png" alt=""/>
-                <img src="../../public/img/vendor-tekgraf.png" alt=""/>
-                </div>
+      <div className="row px-xl-5 d-flex">
+        <div className="vendor-item d-flex justify-content-between">
+          <img src="/img/vendor-yuca.png" alt="Yuca" />
+          <img src="/img/vendor-aremar.png" alt="Aremar" />
+          <img src="/img/vendor-moran.png" alt="Moran" />
+          <img src="/img/vendor-graphic.png" alt="Graphic" />
+          <img src="/img/vendor-tekgraf.png" alt="Tekgraf" />
         </div>
+      </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default Proveedores
+export default Proveedores;
