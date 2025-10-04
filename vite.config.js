a9db0ac752a7f4b0c779128fe75ca1cbd6b9a2e6
Daviel_Sanchez <Davielsanchez14@gmail.com>
2025-10-04 17:56:58 -0400

    Eliminar console.logs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/',
-  plugins: [react()]
-})
+export default defineConfig(({ mode }) => ({
+    base: '/',
+    plugins: [react()],
+    build: {
+        minify: 'terser', // usa Terser para poder eliminar logs
+        terserOptions: {
+            compress: {
+                drop_console: mode === 'production',
+                drop_debugger: mode === 'production',
+            },
+        },
+    },
+}))
